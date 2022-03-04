b1d46ba33aee33235eafb527d7a60af529df9c5e
matheusmenezs <matheushm1798@gmail.com>
2022-03-04 08:29:32 -0300

    feat: create email validation

--- src/controller/employee.ts
+++ src/controller/employee.ts
@@ -1,26 +1,34 @@
 import { getManager } from "typeorm";
 import { Employee } from "../entity/Employee";
 
-export class employeeController {  
-   async save (employee: Employee) {
-       const employeeSave = await getManager().save(employee);
-       return employeeSave;
-   }
-   async listEmployees (){
-       const employees = await getManager().find(Employee);
-       return employees;
-   }    
-   async getEmployee (id: string){
-       const employee = await getManager().findOne(Employee, id);
-       return employee;
-   }
-   async getQuestionsEmployee(id: number){
-       const employee = await getManager().findOne(Employee, id, {
-           relations: ['questions']});
-           
-        if(employee)
+export class employeeController {
+    async validate(employee: Employee) {
+        const emailExists = await getManager().findOne(Employee, { email: `${employee.email}` })
+        if (emailExists) {
+            return { error: "This email is already in use" };
+        } else {
+            return true
+        }
+    }
+    async save(employee: Employee) {
+        const employeeSave = await getManager().save(employee);
+        return employeeSave;
+    }
+    async listEmployees() {
+        const employees = await getManager().find(Employee);
+        return employees;
+    }
+    async getEmployee(id: string) {
+        const employee = await getManager().findOne(Employee, id);
+        return employee;
+    }
+    async getQuestionsEmployee(id: number) {
+        const employee = await getManager().findOne(Employee, id, {
+            relations: ['questions']
+        });
+        if (employee)
             return employee.questions;
         else
             return null;
-   }
+    }
 }
--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -6,39 +6,40 @@ import { validate } from 'class-validator';
 export const routerEmployee = Router();
 const employeeCtrl = new employeeController;
 
-routerEmployee.post('/', async (request:Request, response:Response) => {
-    const {name, email, password, role} = request.body;
+routerEmployee.post('/', async (request: Request, response: Response) => {
+    const { name, email, password, role } = request.body;
     const newEmployee = new Employee(name, email, password, role);
 
     const errors = await validate(newEmployee);
-    
-    if(errors.length == 0){
+    const validateEmail = await employeeCtrl.validate(newEmployee)
+
+    if (errors.length == 0 && validateEmail == true) {
         const saveEmployee = await employeeCtrl.save(newEmployee);
-        response.status(200).json(saveEmployee);
-    }else{
-        response.status(400).json(errors);
+        response.status(200).json(saveEmployee)
+    } else {
+        response.status(400).json({ errors, validateEmail });
     }
 });
 
-routerEmployee.get('/', async (request:Request, response:Response) => {
+routerEmployee.get('/', async (request: Request, response: Response) => {
     const Employees = await employeeCtrl.listEmployees();
     response.status(200).json(Employees);
 });
 
-routerEmployee.get('/search/:id', async (request:Request, response:Response) => {
-    const {id} = request.params;
+routerEmployee.get('/search/:id', async (request: Request, response: Response) => {
+    const { id } = request.params;
     const employee = await employeeCtrl.getEmployee(id);
     response.status(200).json(employee);
 });
 
-routerEmployee.get('/questions/:idEmployee', async (request:Request, response:Response) => {
+routerEmployee.get('/questions/:idEmployee', async (request: Request, response: Response) => {
     const idEmployee = parseInt(request.params.idEmployee);
     const questions = await employeeCtrl.getQuestionsEmployee(idEmployee);
-   
-    if(questions){
+
+    if (questions) {
         response.status(200).json(questions);
-    }else{
-        response.status(400).json({message: "Not found questions"});
+    } else {
+        response.status(400).json({ message: "Not found questions" });
     }
-    
+
 })

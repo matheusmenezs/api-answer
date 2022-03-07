65fea9f1bc11ec351d60f0cebf6b8864f0fb33ac
matheusmenezs <matheushm1798@gmail.com>
2022-03-07 07:13:39 -0300

    style: format the script

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import { createConnection } from "typeorm";
 import { app } from "./app";
 
 const port = 3000;
-
 const server = app.listen(port, () => console.log(`App Listening in port: 3000`));
 
 process.on('SIGINT', () => {

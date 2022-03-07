970b56f5a9b3da02217f004327d54c86afb23da3
matheusmenezs <matheushm1798@gmail.com>
2022-03-07 07:25:21 -0300

    refactor: change validation logic to use express validator

--- src/routes/answer.ts
+++ src/routes/answer.ts
@@ -1,21 +1,21 @@
 import { Request, Response, Router } from "express";
-import { getRepository } from "typeorm";
+import { validationResult } from "express-validator";
 import { answerCtrl } from "../controller/answer";
 import { Answer } from "../entity/Answer";
-
+import { answerValidator } from "../validations/answerValidator";
 
 export const routerAnswer = Router()
 const ctrlAnswer = new answerCtrl
 
-routerAnswer.post('/', async (request: Request, response:Response) => {
-    const {description, employeeId, questionId} = request.body
-    const newAnswer = new Answer(description, employeeId, questionId)
+routerAnswer.post('/', answerValidator, async (request: Request, response: Response) => {
+    const { description, employeeId, questionId } = request.body
+    const errors = validationResult(request)
 
-    const saveAnswer = await ctrlAnswer.save(newAnswer)
-
-    if(saveAnswer){
+    if (errors.isEmpty()) {
+        const newAnswer = new Answer(description, employeeId, questionId)
+        const saveAnswer = await ctrlAnswer.save(newAnswer)
         response.status(200).json(saveAnswer)
-    }else{
-        response.status(400).json(saveAnswer)
+    } else {
+        response.status(400).json(errors)
     }
 })
--- src/routes/question.ts
+++ src/routes/question.ts
@@ -1,22 +1,21 @@
 import { Router, Request, Response } from "express";
 import { Question } from "../entity/Question";
 import { questionController } from "../controller/question";
-import { employeeController } from "../controller/employee";
-
+import { questionValidator } from "../validations/questionValidator";
+import { validationResult } from "express-validator";
 
 export const routerQuestion = Router();
-const questionCtrl = new questionController
-const employeeCtrl = new employeeController
+const questionCtrl = new questionController;
 
-routerQuestion.post('/', async (request:Request, response:Response) => {
-    const {employeeId, number, description} = request.body;
-    const employee = await employeeCtrl.getEmployee(employeeId);
-    if(employee){
-        const question = new Question(number, description, employeeId);
+routerQuestion.post('/', questionValidator, async (request: Request, response: Response) => {
+    const { description, employeeId } = request.body;
+    const errors = validationResult(request)
+
+    if (errors.isEmpty()) {
+        const question = new Question(description, employeeId);
         const saveQuestion = await questionCtrl.save(question);
-        return response.status(200).json({questionSaved: saveQuestion, message: 'Question created successfully'})
-    }else{
-        return response.status(404).json({message: 'Employee not found!'})
+        return response.status(200).json({ question: saveQuestion, message: 'Question created successfully' });
+    } else {
+        return response.status(404).json(errors);
     }
-
 })

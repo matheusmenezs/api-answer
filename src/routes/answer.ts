40f43d04abe5d43224ccfcc3198d2e1222337561
matheusmenezs <matheushm1798@gmail.com>
2022-03-14 00:09:51 -0300

    refactor: refactor code to get employeeId from token

--- src/routes/answer.ts
+++ src/routes/answer.ts
@@ -8,11 +8,12 @@ export const routerAnswer = Router()
 const ctrlAnswer = new answerCtrl
 
 routerAnswer.post('/', answerValidator, async (request: Request, response: Response) => {
-    const { description, employeeId, questionId } = request.body
+    const { description, questionId } = request.body
     const errors = validationResult(request)
 
     if (errors.isEmpty()) {
-        const newAnswer = new Answer(description, employeeId, questionId)
+        const newAnswer = new Answer(description, questionId)
+        newAnswer.employee = request.employeeId
         const saveAnswer = await ctrlAnswer.save(newAnswer)
         response.status(200).json(saveAnswer)
     } else {
